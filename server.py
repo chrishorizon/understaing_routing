e5c294007f225132107c0c89994cae3f3c8596b2
Chris <christhehorizon@gmail.com>
2021-08-05 14:34:49 -0700

    first draft

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<user>')
+def hi_user(user):
+    print(user)
+    return "Hi " + user + "!"
+
+@app.route('/repeat/<int:number>/<string:word>')
+def multiply(number, word):
+    print(number)
+    print(word)
+    return word * number
+
+if __name__ == '__main__':
+    app.run(debug=True)
